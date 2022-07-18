e9cf119b24faded24ac227a425af849e0fef9261
Danil <d.danil.s.96@gmail.com>
2022-07-18 20:45:22 +0500

    Add index route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const indexRouter = express.Router();
 
 indexRouter.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.render("index", { title: "Home page" })
 });
 
 module.exports = indexRouter;
