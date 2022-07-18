a73ec45268820c3a99e09db65754815dabf856f8
Danil <d.danil.s.96@gmail.com>
2022-07-18 18:52:24 +0500

    Add error view

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const path = require("path");
+const createError = require("http-errors");
 
 const app = express();
 const indexRouter = require("./routes/index");
@@ -16,6 +17,18 @@ app.use("./static", express.static((path.join(__dirname, "public"))));
 
 // routes setup
 app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "dev" ? err : {};
+
+    res.status(err.status || 500);
+    res.render("error");
+})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}, pid: ${process.pid}`);
