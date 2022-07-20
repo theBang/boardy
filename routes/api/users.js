12b454e7d8f07a1b5fc4edea1c00293b15e92a8f
Danil <d.danil.s.96@gmail.com>
2022-07-20 17:45:54 +0500

    Add find by id route

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,14 @@ usersRouter.get("/", async (req, res) => {
     const dataSource = res.app.get("dataSource");
     const userRepo = dataSource.getRepository(User);
     const users = await userRepo.find();
-    res.json(users);
+    res.send(users);
 });
+
+usersRouter.get("/:id", async (req, res) => {
+    const user = await res.app.get("dataSource").getRepository(User).findOneBy({
+        id: parseInt(req.params.id, 10)
+    });
+    res.send(user);
+})
 
 module.exports = usersRouter;
