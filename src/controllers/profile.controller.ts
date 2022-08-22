c63d257b2dd246019d29d0ba178a3dfd6751b73d
Danil <d.danil.s.96@gmail.com>
2022-08-22 15:40:54 +0500

    fix export

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,14 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { Repository } from "typeorm";
 import createHttpError from "http-errors";
-import { User } from "../entity/User";
 import { Profile } from "../entity/Profile";
 
 function getProfileRepo(res: Response): Repository<Profile> {
     return res.app.get("dataSource").getRepository(Profile);
 }
 
-export async function getUserProfile(req: Request, res: Response) {
+async function getUserProfile(req: Request, res: Response) {
     const profile = await getProfileRepo(res).findOneBy({
         user: {
             id: parseInt(req.params.id, 10)
@@ -17,17 +16,17 @@ export async function getUserProfile(req: Request, res: Response) {
     res.send(profile);
 }
 
-export async function updateUserProfile(req: Request, res: Response, next: NextFunction) {
+async function updateUserProfile(req: Request, res: Response, next: NextFunction) {
     const profileRepo = getProfileRepo(res);
-    const user = await profileRepo.findOneBy({
+    const profile = await profileRepo.findOneBy({
         user: {
             id: parseInt(req.params.id, 10)
         }
     });
 
-    if (user) {
-        profileRepo.merge(user, req.body);
-        const results = await profileRepo.save(user);
+    if (profile) {
+        profileRepo.merge(profile, req.body);
+        const results = await profileRepo.save(profile);
         res.send(results);
     }
     else {
